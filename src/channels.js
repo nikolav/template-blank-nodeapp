216232f7bbe2acb1c8e811073641c7545822bcae
nikolav <admin@nikolav.rs>
2022-07-12 00:46:14 +0100

    +authentication

--- src/channels.js
+++ src/channels.js
@@ -10,32 +10,32 @@ module.exports = function (app) {
     app.channel("@public").join(connection);
   });
 
-  // app.on('login', (authResult, { connection }) => {
-  //   // connection can be undefined if there is no
-  //   // real-time connection, e.g. when logging in via REST
-  //   if(connection) {
-  //     // Obtain the logged in user from the connection
-  //     // const user = connection.user;
+  app.on("login", (authResult, { connection }) => {
+    // connection can be undefined if there is no
+    // real-time connection, e.g. when logging in via REST
+    if (connection) {
+      // Obtain the logged in user from the connection
+      // const user = connection.user;
 
-  //     // The connection is no longer anonymous, remove it
-  //     app.channel('anonymous').leave(connection);
+      // The connection is no longer anonymous, remove it
+      // app.channel('anonymous').leave(connection);
 
-  //     // Add it to the authenticated user channel
-  //     app.channel('authenticated').join(connection);
+      // Add it to the authenticated user channel
+      app.channel("@user").join(connection);
 
-  //     // Channels can be named anything and joined on any condition
+      // Channels can be named anything and joined on any condition
 
-  //     // E.g. to send real-time events only to admins use
-  //     // if(user.isAdmin) { app.channel('admins').join(connection); }
+      // E.g. to send real-time events only to admins use
+      // if(user.isAdmin) { app.channel('admins').join(connection); }
 
-  //     // If the user has joined e.g. chat rooms
-  //     // if(Array.isArray(user.rooms)) user.rooms.forEach(room => app.channel(`rooms/${room.id}`).join(connection));
+      // If the user has joined e.g. chat rooms
+      // if(Array.isArray(user.rooms)) user.rooms.forEach(room => app.channel(`rooms/${room.id}`).join(connection));
 
-  //     // Easily organize users by email and userid for things like messaging
-  //     // app.channel(`emails/${user.email}`).join(connection);
-  //     // app.channel(`userIds/${user.id}`).join(connection);
-  //   }
-  // });
+      // Easily organize users by email and userid for things like messaging
+      // app.channel(`emails/${user.email}`).join(connection);
+      // app.channel(`userIds/${user.id}`).join(connection);
+    }
+  });
 
   // eslint-disable-next-line no-unused-vars
   app.publish((data, hook) => {
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const main = require('./main/main.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(main);
+  app.configure(users);
 };
