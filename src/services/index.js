16d4bdc3de9df973dfde6228663f437a7cc33907
Nikola Vukovic <nikolav@users.noreply.github.com>
2022-08-18 12:30:06 +0200

    messages service

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const main = require('./main/main.service.js');
 const users = require('./users/users.service.js');
+const messages = require('./messages/messages.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(main);
   app.configure(users);
+  app.configure(messages);
 };
